10bafe48effb50ad4b6c49c90dd5faa09159c4fb
Somtozech <ezechinnaemeka@gmail.com>
2019-08-24 22:18:45 +0100

    Added routing

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// const Server = require('./lib/server');
+const app = require('./lib/server');
+const port = process.env.PORT || 3000;
+
+app.get('/create', (request, response) => {
+  console.log('getting create');
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  return response.write(JSON.stringify({ Message: 'This is a get request' }));
+});
+
+app.post('/create', (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify({ Message: 'This is a post request' }));
+});
+
+app.put('/create', (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify({ Message: 'This is an update request' }));
+});
+
+app.delete('/create', (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify({ Message: 'This is a delete request' }));
+});
+
+app.listen(port, () => {
+  console.log(`Server Listening at port ${port}`);
+});
